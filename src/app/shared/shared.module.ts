916523b7056d498a91f0b87e3630d8920a9ed288
RevathyRajeevan <revathyrajeevan12345@gmail.com>
2024-08-26 19:53:33 +0530

    removed extra blank lines

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 
-
-
 @NgModule({
   declarations: [
     ErrorMessageComponent
