374380752e060894768da30e71926263c2284017
Sreejith Shaji <sreejithshaji0076@gmail.com>
2024-08-28 20:05:20 +0530

    suggestions were made

--- src/app/features/vendor/vendor-listing/vendor-listing.component.ts
+++ src/app/features/vendor/vendor-listing/vendor-listing.component.ts
@@ -5,27 +5,20 @@ import { Router } from '@angular/router';
 
 
 /**
-* VendorListingComponent
-*
-* This component is responsible for listing all the vendors in the system.
-*
-* Execution Flow
-*
-* The following details will be displayed against each vendor:
-*
-* - Vendor Name
-* - Vendor Status Badge - A "Pending" tag is shown for vendors with `isApproved: false`.
-* - Markets
-* - Service Categories
-* - Email
-*
-* Default sort order: Ascending order of vendor names.
-*
-* This screen contains the following actions:
-*
-* - Add New Vendor: Navigates to the vendor creation page when the "Add New Vendor" button is clicked.
-* - View Vendor Details: The user can click on a row to navigate to the detailed view of that vendor.
-*/
+ * VendorListingComponent
+ * This component is responsible for listing all the vendors in the system.
+ * Execution Flow
+ * The following details will be displayed against each vendor:
+    * Vendor Name
+    * Vendor Status Badge - A "Pending" tag is shown for vendors with `isApproved: false`.
+    * Markets
+    * Service Categories
+    * Email
+ * Default sort order: Ascending order of vendor names.
+ * This screen contains the following actions:
+    * Add New Vendor: Navigates to the vendor creation page when the "Add New Vendor" button is clicked.
+    * View Vendor Details: The user can click on a row to navigate to the detailed view of that vendor.
+ */
 
 @Component({
   selector: 'app-vendor-listing',
