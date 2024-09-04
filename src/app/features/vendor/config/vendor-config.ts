ffbf7c442f295ed89ec1950d722710a9354cc36a
Sreejith Shaji <sreejithshaji0076@gmail.com>
2024-09-04 17:27:32 +0530

    PR suggestions

--- src/app/features/vendor/config/vendor-config.ts
+++ src/app/features/vendor/config/vendor-config.ts
@@ -12,7 +12,7 @@ export class Constants {
     constructor() {
       this.phonePattern = /^[1-9][0-9]{9}$/;
       this.urlPattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}(\.[a-z]{2,6})\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
-      this.confirmCancelMessage  ="Are you sure you want to cancel the changes?";
-      this.confirmApprovalMessage  ="Are you sure you want to approve this vendor?";
+      this.confirmCancelMessage  ='Are you sure you want to cancel the changes?';
+      this.confirmApprovalMessage  ='Are you sure you want to approve this vendor?';
     }
 }
